6094e9491dd7078a514f292c3b3dd15b3b21f66e
choisoobong <devcsb119@gmail.com>
2022-05-02 13:44:27 +0900

    활용2 - 주문 조회V2 - 엔티티를 DTO로 변환
    엔티티를 DTO로 변환시, 겉 껍데기만 DTO로 감싸는 것이 아닌, 모든 연관관계로 엮여있는 Entity에 대해 모두 DTO로 변환해서 사용해야함을 정확히 인지하고 그 이유를 이해.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,5 +39,59 @@ public class OrderApiController {
             orderItems.stream().forEach(o -> o.getItem().getName()); //OrderItem 내부에 item이 있으므로 그것의 이름을 얻어와야하므로 프록시 초기화.
         }
         return all;
+    }
+
+    /*
+    *  V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+    * - 트랜잭션 안에서 지연 로딩 필요
+    * */
+    //엔티티를 외부에 노출시키지 말라는 뜻은, 단순히 Dto로 감싸는 걸로 끝내라는 말이 아니라, 의존관계에 엮인 모든 Entity를 Dto로 전부 바꿔서 써야한다는 뜻이다.
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch()); //Entity에서 orders 조회 (Entity 타입)
+        List<OrderDto> result = orders.stream()  //Entity에서 Dto로 변환
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    //    No serializer found for class jpabook.jpashop.api.OrderApiController$OrderDto and no properties discovered to create BeanSerializer
+    //    오류 중 no properties는 대부분 getter setter 가 없어서 발생.
+    @Data
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+//        private List<OrderItem> orderItems; //지금은 OrderItem의 Entity를 반환하고 있다. 하지만 OrderItem 도 Dto로 다 바꿔야한다.
+        private List<OrderItemDto> orderItems; //Dto형태로 받음.
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());  //OrderItem을 Dto로 변환
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName; //상품 명
+        private int orderPrice;  //주문 가격
+        private int count;  //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
